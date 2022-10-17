cd3ac5a1ac17ef8fa6d20b92fcfab5fb6a863d0f
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-10-17 17:29:23 -0300

    SignIn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,12 @@
 import './global/styles/tailwind.css'
 import './app.css'
-import { Logo } from './components/logo'
-import { Heading } from './components/heading'
-import { Text } from './components/text'
-import { TextInput } from './components/textInput'
-import { Envelope } from 'phosphor-react'
-import { Checkbox } from './components/checkbox'
-import { Button } from './components/button'
+
+import { SignIn } from './pages/signIn'
 
 function App() {
 
   return (
-    <div className="App">
-      <header className='header'>  
-        <Logo />
-
-        <Heading size='lg' className='mt-4'>
-          ignite Lab
-        </Heading>
-
-        <Text colorText='text-gray-400' size="lg" className="mt-1">
-            Faça login e comece a usar!
-        </Text>
-      </header>
-
-      <form action="" className='form'>
-        <label htmlFor="email" className='label'>
-          <Text className='font-semibold' >Endereço de e-mail</Text>
-          <TextInput.Root>
-            <TextInput.Icon>
-              <Envelope />
-            </TextInput.Icon>
-
-            <TextInput.Input type='email' id='email' placeholder='Digite seu e-mail' />
-          </TextInput.Root>
-        </label>
-
-        <label htmlFor="password" className='label'>
-          <Text className='font-semibold' >Sua senha</Text>
-          <TextInput.Root>
-            <TextInput.Icon>
-              <Envelope />
-            </TextInput.Icon>
-
-            <TextInput.Input type='password' id='password' placeholder='*******' />
-          </TextInput.Root>
-        </label>
-
-        <label htmlFor="remember" className="remember">
-          <Checkbox id='remember'/>
-          <Text size="sm" colorText="text-gray-200">Lembrar de mim por 30 dias</Text>
-        </label>
-
-        <Button type='submit' className='mt-4'>Entrar na plataforma</Button>
-      </form>
-
-      <footer className='footer'>
-        <Text asChild size='sm'>
-          <a href="" className='link'>Esqueceu sua senha?</a>
-        </Text>
-        <Text asChild size='sm'>
-          <a href="" className='link'>Não possui conta? Crie uma agora!</a>
-        </Text>
-      </footer>
-    </div>
+    <SignIn />
   )
 }
 
